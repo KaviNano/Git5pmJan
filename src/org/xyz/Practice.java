e26f4216ab3dbaf92003b13865abc1d959640b01
KaviNano <kavinano100@gmail.com>
2023-01-13 09:00:33 +0530

    sam done

--- src/org/xyz/Practice.java
+++ src/org/xyz/Practice.java
@@ -2,6 +2,12 @@ package org.xyz;
 
 public class Practice {
 
+	int a = 200;
+
+	private void samMethod() {
+		System.out.println("sam");
+	}
+
 	public static void main(String[] args) {
 
 		int a = 100, b = 200;
